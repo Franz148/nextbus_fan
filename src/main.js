ca515cacb43393471bc1b627c32a31d7be0f594e
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-05-21 16:16:56 +0200

    Inclusione Vue Material

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueMaterial from "vue-material";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueMaterial);
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount("#app");
