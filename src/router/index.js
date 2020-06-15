e871791dbde88d0f6a0088331cca0d80b054e46a
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-15 16:29:55 +0200

    Aggiunta route preferiti

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Preferiti from "../views/Preferiti.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-    { path: "/", redirect: "/pianifica" }
+    { path: "/", redirect: "/pianifica" },
+    { path: "/preferiti", component: Preferiti }
 ];
 
 const router = new VueRouter({
