f520710169290758a97d84d9d9a028256c51378b
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-15 16:48:31 +0200

    Inizio grafica preferiti

---
+++ src/views/Preferiti.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <md-card>
+        <md-card-header>
+          <div class="md-title">Card without hover effect</div>
+        </md-card-header>
+
+        <md-card-content>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.</md-card-content>
+
+        <md-card-actions>
+          <md-button>Action</md-button>
+          <md-button>Action</md-button>
+        </md-card-actions>
+      </md-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
