d2e63a61480ce0f8a906e2ead89b29dc283618f6
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-15 18:23:29 +0200

    Fine gestione "scheletro" preferiti

--- src/views/Preferiti.vue
+++ src/views/Preferiti.vue
@@ -2,32 +2,38 @@
   <div>
     <div class="md-layout md-alignment-center-center">
       <md-card class="md-layout-item md-xsmall-size-90 md-small-size-80 md-size-60" md-with-hover>
-        <md-card-actions class="md-layout md-primary">
-          <div class="md-layout-item md-size-90"></div>
-          <md-menu md-size="small" md-align-trigger>
+        <md-card-header>
+          <md-card-header-text>
+            <div class="md-title">Lista dei preferiti</div>
+            <div class="md-subhead">
+              Fermate e linee preferite
+              <md-icon>favorite</md-icon>verranno visualizzate qui
+            </div>
+          </md-card-header-text>
+
+          <md-menu md-size="medium" md-align-trigger>
             <md-button class="md-icon-button" md-menu-trigger>
-              <md-icon>filter_list</md-icon>
+              <md-icon>more_vert</md-icon>
             </md-button>
 
             <md-menu-content>
               <md-menu-item>Filtro 1</md-menu-item>
               <md-menu-item>Filtro 2</md-menu-item>
-              <md-menu-item>Filtro 3</md-menu-item>
             </md-menu-content>
           </md-menu>
-        </md-card-actions>
-        <md-card-content>
+        </md-card-header>
+
+        <md-card-content class="content">
           <md-tabs class="tabs" md-alignment="centered">
             <md-tab id="tab-preferiti-fermate" md-label="Fermate">
               <p>
-                Fermate Favorites tab <br><br>
-                Maiores, dolorum. Beatae, optio tempore fuga odit aperiam velit, consequuntur magni inventore sapiente alias sequi odio qui harum dolorem sunt quasi corporis.
+                Fermate Favorites tab
+                
               </p>
             </md-tab>
             <md-tab id="tab-preferiti-linee" md-label="Linee">
               <p>
-                Linee Favorites tab <br><br>
-                Maiores, dolorum. Beatae, optio tempore fuga odit aperiam velit, consequuntur magni inventore sapiente alias sequi odio qui harum dolorem sunt quasi corporis.
+                Linee Favorites tab
               </p>
             </md-tab>
           </md-tabs>
@@ -42,4 +48,7 @@ export default {};
 </script>
 
 <style scoped>
+.content {
+  padding: 0;
+}
 </style>
