2c1e32c6ac6bae0fb86eeb984898d5513747434d
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-15 16:55:20 +0200

    layout card

--- src/views/Preferiti.vue
+++ src/views/Preferiti.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <div>
-      <md-card>
+    <div class="md-layout md-gutter md-alignment-center">
+      <md-card class="md-layout-item md-small-size-50 md-medium-size-80 md-xsmall-size-100">
         <md-card-header>
           <div class="md-title">Card without hover effect</div>
         </md-card-header>
