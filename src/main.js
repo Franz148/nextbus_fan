e04596750ed35df2e75e6b92747dd333d2a367f0
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-16 16:51:23 +0200

    Inserimento tema

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueMaterial from "vue-material";
+import 'vue-material/dist/vue-material.min.css'
+
+//Tema di default vue material
+import 'vue-material/dist/theme/default.css'
 
 Vue.config.productionTip = false;
 
