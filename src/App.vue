e7cd5dad3adaa64b6599ebeab704d8d3b13df812
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-16 17:12:30 +0200

    Inizializzazione App.vue comune

--- src/App.vue
+++ src/App.vue
@@ -4,55 +4,23 @@
       <md-app-toolbar class="md-large md-dense md-primary">
         <div class="md-toolbar-row">
           <div class="md-toolbar-section-start">
-            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
-              <md-icon>menu</md-icon>
-            </md-button>
-
-            <span class="md-title">My Title</span>
-          </div>
-
-          <div class="md-toolbar-section-end">
             <md-button class="md-icon-button">
-              <md-icon>more_vert</md-icon>
+              <md-icon>directions_bus</md-icon>
             </md-button>
+
+            <span class="md-title">NextBus</span>
           </div>
         </div>
 
         <div class="md-toolbar-row">
-          <md-tabs class="md-primary">
-            <md-tab id="tab-home" md-label="Home"></md-tab>
-            <md-tab id="tab-pages" md-label="Pages"></md-tab>
-            <md-tab id="tab-posts" md-label="Posts"></md-tab>
-            <md-tab id="tab-favorites" md-label="Favorites"></md-tab>
+          <md-tabs class="md-primary" md-alignment="centered">
+            <md-tab id="tab-pianifica" md-label="Pianifica"></md-tab>
+            <md-tab id="tab-fermate" md-label="Fermate"></md-tab>
+            <md-tab id="tab-linee" md-label="Linee"></md-tab>
+            <md-tab id="tab-preferiti" md-label="Preferiti"></md-tab>
           </md-tabs>
         </div>
       </md-app-toolbar>
-
-      <md-app-drawer :md-active.sync="menuVisible">
-        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
-
-        <md-list>
-          <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
 
       <md-app-content>
         <router-view></router-view>        
@@ -63,9 +31,13 @@
 
 <style>
 
-.app {
+.md-app {
   width: 100%;
   height: 100vh;
+}
+
+.md-tabs {
+  width: 100%;
 }
 
 </style>
@@ -73,7 +45,6 @@
 <script>
 export default {
   data: () => ({
-    menuVisible: false
   })
 };
 </script>
