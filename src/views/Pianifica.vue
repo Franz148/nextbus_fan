ddc4b84794fa751e46c042206982b28e1a269be2
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-17 16:43:32 +0200

    Creazione struttura Pianifica

--- src/views/Pianifica.vue
+++ src/views/Pianifica.vue
@@ -1,131 +1,73 @@
 <template>
   <div class="md-layout">
-    <md-card class="md-layout-item">
-      <md-card-header>
-        <div class="md-title">Pianifica il tuo viaggio</div>
-      </md-card-header>
+    <form novalidate class="md-layout-item md-layout md-alignment-center-center">
+      <md-card class="md-layout-item md-size-50 md-small-size-90">
+        <md-card-header>
+          <div class="md-title">Pianifica il tuo viaggio</div>
+        </md-card-header>
 
-      <md-card-content>
-        <form novalidate class="md-layout" @submit.prevent="validateUser">
-          <md-card class="md-layout-item md-size-50 md-small-size-100">
-            <md-card-header>
-              <div class="md-title">Users</div>
-            </md-card-header>
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-size-80">
+                <label class="md-subheading">Partenza</label>
+                <md-autocomplete
+                  v-model="selectedEmployee"
+                  :md-options="employees"
+                  :md-open-on-focus="false"
+                >
+                  <label>Inserisci un indirizzo</label>
+                </md-autocomplete>
+            </div>
 
-            <md-card-content>
-              <div class="md-layout md-gutter">
-                <div class="md-layout-item md-small-size-100">
-                  <md-field :class="getValidationClass('firstName')">
-                    <label for="first-name">First Name</label>
-                    <md-input
-                      name="first-name"
-                      id="first-name"
-                      autocomplete="given-name"
-                      v-model="form.firstName"
-                      :disabled="sending"
-                    />
-                    <span
-                      class="md-error"
-                      v-if="!$v.form.firstName.required"
-                    >The first name is required</span>
-                    <span
-                      class="md-error"
-                      v-else-if="!$v.form.firstName.minlength"
-                    >Invalid first name</span>
-                  </md-field>
-                </div>
+            <div class="md-layout-item md-size-80">
+              <label class="md-subheading">Arrivo</label>
+                <md-autocomplete
+                  v-model="selectedEmployee"
+                  :md-options="employees"
+                  :md-open-on-focus="false"
+                >
+                  <label>Inserisci un indirizzo</label>
+                </md-autocomplete>
+            </div>
 
-                <div class="md-layout-item md-small-size-100">
-                  <md-field :class="getValidationClass('lastName')">
-                    <label for="last-name">Last Name</label>
-                    <md-input
-                      name="last-name"
-                      id="last-name"
-                      autocomplete="family-name"
-                      v-model="form.lastName"
-                      :disabled="sending"
-                    />
-                    <span
-                      class="md-error"
-                      v-if="!$v.form.lastName.required"
-                    >The last name is required</span>
-                    <span class="md-error" v-else-if="!$v.form.lastName.minlength">Invalid last name</span>
-                  </md-field>
-                </div>
-              </div>
+            <div class="md-layout-item md-size-80 md-layout">
+              <label class="md-subheading">Quando</label>
+              
+            </div>
+          </div>
+        </md-card-content>
 
-              <div class="md-layout md-gutter">
-                <div class="md-layout-item md-small-size-100">
-                  <md-field :class="getValidationClass('gender')">
-                    <label for="gender">Gender</label>
-                    <md-select
-                      name="gender"
-                      id="gender"
-                      v-model="form.gender"
-                      md-dense
-                      :disabled="sending"
-                    >
-                      <md-option></md-option>
-                      <md-option value="M">M</md-option>
-                      <md-option value="F">F</md-option>
-                    </md-select>
-                    <span class="md-error">The gender is required</span>
-                  </md-field>
-                </div>
-
-                <div class="md-layout-item md-small-size-100">
-                  <md-field :class="getValidationClass('age')">
-                    <label for="age">Age</label>
-                    <md-input
-                      type="number"
-                      id="age"
-                      name="age"
-                      autocomplete="age"
-                      v-model="form.age"
-                      :disabled="sending"
-                    />
-                    <span class="md-error" v-if="!$v.form.age.required">The age is required</span>
-                    <span class="md-error" v-else-if="!$v.form.age.maxlength">Invalid age</span>
-                  </md-field>
-                </div>
-              </div>
-
-              <md-field :class="getValidationClass('email')">
-                <label for="email">Email</label>
-                <md-input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autocomplete="email"
-                  v-model="form.email"
-                  :disabled="sending"
-                />
-                <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
-                <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
-              </md-field>
-            </md-card-content>
-
-            <md-progress-bar md-mode="indeterminate" v-if="sending" />
-
-            <md-card-actions>
-              <md-button type="submit" class="md-primary" :disabled="sending">Create user</md-button>
-            </md-card-actions>
-          </md-card>
-
-          <md-snackbar :md-active.sync="userSaved">The user {{ lastUser }} was saved with success!</md-snackbar>
-        </form>
-      </md-card-content>
-
-      <md-card-actions>
-        <md-button>Action</md-button>
-        <md-button>Action</md-button>
-      </md-card-actions>
-    </md-card>
+        <md-card-actions>
+          <md-button>Annulla</md-button>
+          <md-button type="submit" class="md-primary">Cerca</md-button>
+        </md-card-actions>
+      </md-card>
+    </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    selectedEmployee: null,
+    employees: [
+      "Jim Halpert",
+      "Dwight Schrute",
+      "Michael Scott",
+      "Pam Beesly",
+      "Angela Martin",
+      "Kelly Kapoor",
+      "Ryan Howard",
+      "Kevin Malone",
+      "Creed Bratton",
+      "Oscar Nunez",
+      "Toby Flenderson",
+      "Stanley Hudson",
+      "Meredith Palmer",
+      "Phyllis Lapin-Vance"
+    ]
+  })
+};
 </script>
 
 <style>
