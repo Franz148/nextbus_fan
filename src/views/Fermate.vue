52f2a62ad06e629919583e6bd4910ed6cb35eb00
nicolemariz <61781935+nicolemariz@users.noreply.github.com>
2020-06-18 15:43:12 +0200

    Branch Nicole
    Ecco qui le liste

--- src/views/Fermate.vue
+++ src/views/Fermate.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-      Pagina fermate
+  <div class="md-layout md-gutter md-alignment-center-center">
+    <md-list class="md-layout-item md-size-50">
+      <md-divider></md-divider>
+      <md-list-item>
+        <md-icon>directions_bus</md-icon>
+
+        <span class="md-list-item-text">Piazza Dante Stazione FS</span>
+
+        <md-icon class="md-primary">accessible</md-icon>
+        <md-icon class="md-primary">favorite_border</md-icon>
+      </md-list-item>
+
+      <md-divider></md-divider>
+    </md-list>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/views/Linee.vue
+++ src/views/Linee.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-      Pagina linee
+  <div class="md-layout md-gutter md-alignment-center-center">
+    <md-list class="md-double-line">
+      <md-divider></md-divider>
+      <md-list-item >
+        <md-avatar>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NYCS-bull-trans-1.svg/1200px-NYCS-bull-trans-1.svg.png"
+          />
+        </md-avatar>
+        <div class="md-list-item-text">
+          <span>Linea Uno</span>
+          <span>Direzione Sopramonte</span>
+        </div>
+      </md-list-item>
+      <md-divider></md-divider>
+    </md-list>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
