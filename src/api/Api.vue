6a1ea00e304b6c11b292915aab039b33069ed329
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-18 15:41:51 +0200

    modifiche per nuove funzioni

--- src/api/Api.vue
+++ src/api/Api.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <md-list>
-      <div v-for="linea in linee" :key="linea.id.id">
+      <div>
         <md-list-item>
           <div class="md-list-item-text">
-            <span>{{linea.routeLongName}}</span>
-            <span>{{linea.routeShortName}}</span>
+            <span>{{}}</span>
           </div>
         </md-list-item>
         <md-divider></md-divider>
@@ -19,15 +18,48 @@ import Functions from "./functions.js";
 
 export default {
   data: () => ({
-    linee: []
+    linea: []
+    /*idFermate: [],
+    nomeFermate: [],
+    viaggi: []*/
   }),
+  
+  methods: {
+    /*funzioneProva(s){
+      if(s == ""){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }*/
+  },
+
   created: function() {
-    Functions.getLinee(12)
+    /*Functions.getLinee(12)
       .then(results => {
         this.linee = results.data;
       })
       .catch(error => {
         console.error(error);
+      });*/
+
+      /*Functions.getLineaSingolaConOrario(12, "10R")
+      .then(results => {
+        this.idFermate = results.data.stopIds;
+        this.nomeFermate = results.data.stopNames;
+        this.viaggi = results.data.trips[0].stopTimes;
+      })
+      .catch(error => {
+        console.log(error);
+      });*/
+
+      Functions.getLineaSingolaAccessibilita(12, "10R")
+      .then(results => {
+        this.linea = results.data;
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 };
