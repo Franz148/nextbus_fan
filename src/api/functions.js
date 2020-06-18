7c3ad4be8703adb487279a9ba812c8d5c07b2bf6
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-18 15:41:39 +0200

    nuove funzioni getLinee, getLineaOra e getLineaAcc

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,11 +1,34 @@
 import axios from "axios";
 
 export default {
-    helloWorld() {
-        alert("ciao mondo!");
+    getLinee(agencyId) {
+        //this.linee = results.data;
+
+        return axios.get("https://os.smartcommunitylab.it/core.mobility/getroutes/" + agencyId);
     },
 
-    getLinee(agencyId) {
-        return axios.get("https://os.smartcommunitylab.it/core.mobility/getroutes/" + agencyId);
+    getLineaSingolaConOrario(agencyId, routeId) {
+        /*this.idFermate = results.data.stopIds;
+        this.nomeFermate = results.data.stopNames;
+        this.viaggi = results.data.trips[0].stopTimes;*/
+
+        //     <md-list>
+        //   <div v-for="n in idFermate.length" :key="idFermate[n]">
+        //     <md-list-item v-if="funzioneProva(viaggi[n])">
+        //       <div class="md-list-item-text">
+        //         <span>{{idFermate[n]}}</span>
+        //         <span>{{nomeFermate[n]}}</span>
+        //         <span>{{viaggi[n]}}</span>
+        //       </div>
+        //     </md-list-item>
+        //     <md-divider></md-divider>
+        //   </div>
+        // </md-list>
+
+        return axios.get("https://os.smartcommunitylab.it/core.mobility/timetable/" + agencyId + "/" + routeId);
+    },
+
+    getLineaSingolaAccessibilita(agencyId, routeId) {
+        return axios.get("https://os.smartcommunitylab.it/core.mobility/getstops/" + agencyId + "/" + routeId);
     }
 }
