a34018eedb18b1324a2e439c3312eadab7e342b7
alberto-testa <61781951+alberto-testa@users.noreply.github.com>
2020-06-19 10:03:11 +0200

    prova lista

--- src/views/FermataSingola.vue
+++ src/views/FermataSingola.vue
@@ -1,12 +1,43 @@
 <template>
+<div class ="md-layout md-alignment-center-center">
+    
+    <h1 class="md-alignment-centered-center"> nomeFermata<!--{{nome}}--> </h1>
+    
+    
+    <md-list class="md-layout">
+        <!--
+        <div v-for="orario in orari" :key="orario.valore">
+            <md-list-item class="md-aligment-centered-center">
+                <span>fff<inserisco valori dati di ogni singolo orario</span>
+            </md-list-item>
+            <md-divider></md-divider>
+        </div>
+        -->
+    <md-list-item>
+        dsrdfcve
+    </md-list-item>
+    <md-divider></md-divider>
+
+
+    </md-list>
+    
+
+</div>
   
 </template>
 
 <script>
-export default {
-
+export default{
+    data: function()
+    {
+        return {
+            orari:[]
+        }
+    }
 }
+
 </script>
+
 
 <style>
 
