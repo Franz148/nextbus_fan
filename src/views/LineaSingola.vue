7cbe79f0576f247c69e1ffd309946c28c28c3d8d
alberto-testa <61781951+alberto-testa@users.noreply.github.com>
2020-06-19 17:03:42 +0200

    ricezione orari linea singola

--- src/views/LineaSingola.vue
+++ src/views/LineaSingola.vue
@@ -1,12 +1,56 @@
 <template>
-  
+
+  <div class="md-layout md-gutter md-alignment-center-center">
+    <md-list class="md-layout-item md-size-50">
+       <div v-for="n in idFermate.length" :key="idFermate[n]">
+             
+             <md-list-item>
+        <md-icon class="md-primary">phone</md-icon>
+
+        <div class="md-list-item-text">
+          <span>{{nomeFermate[n]}}</span>
+          <span>Orario previsto: {{viaggi[n]}}</span>
+        </div>
+ 
+        <md-button class="md-icon-button md-list-action">
+          <md-icon>sms</md-icon>
+        </md-button>
+      </md-list-item>
+             <md-divider></md-divider>
+            </div>
+
+           
+      
+         </md-list>
+  </div>
 </template>
 
 <script>
+import Functions from "../api/functions.js";
 export default {
+    data: () => ({
+    idFermate:[],
+    nomeFermate:[],
+    viaggi:[]
+  }),
+  created: function() {
+      //(idAgency,idRotes)
+    Functions.getLineaSingolaConOrario(12,"10R")
+      .then(results => {
+        this.idFermate = results.data.stopIds;
+        this.nomeFermate = results.data.stopNames;
+
+        //la posizione di results.data.trips è la trip
+        this.viaggi = results.data.trips[1].stopTimes;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
 }
 </script>
+
 
 <style>
 
