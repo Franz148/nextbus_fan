441a0f6cc96ca609370e15d53fa79b71618aa06a
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-19 15:31:36 +0200

    Prova Viaggio.vue

--- src/views/Viaggio.vue
+++ src/views/Viaggio.vue
@@ -1,13 +1,25 @@
 <template>
-  
+  <div class="md-layout md-alignment-center-center">
+    <md-card class="md-layout-item md-size-50 md-small-size-90" md-with-hover>
+      <md-card-header>
+        <div class="md-title">Risultati ricerca</div>
+      </md-card-header>
+
+      <md-card-content>
+
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button>Action</md-button>
+        <md-button>Action</md-button>
+      </md-card-actions>
+    </md-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
