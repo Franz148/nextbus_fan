c0bafd8ff00e85ae2c948d4ce8f14166a56cd98b
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-20 20:24:21 +0200

    Gestione visualizzazione viaggio.vue

--- src/views/Viaggio.vue
+++ src/views/Viaggio.vue
@@ -7,25 +7,35 @@
 
       <md-card-content>
         <md-list md-expand-single="true">
-          <md-list-item md-expand>
-            <md-icon>whatshot</md-icon>
-            <span class="md-list-item-text">News</span>
+          <md-list-item md-expand v-for="viaggio in risultati" :key="viaggio.duration">
+            <md-icon v-if="viaggio.promoted">directions_walk</md-icon>
+            <md-icon v-if="!viaggio.promoted">directions_bus</md-icon>
+
+            <span class="md-list-item-text">
+              Durata
+              <b>{{msToHMS(viaggio.duration)}}</b>
+            </span>
+
+            <span class="md-list-item-text">
+              Partenza
+              <b>{{convertTimestampToTime(viaggio.startime)}}</b>
+            </span>
+
+            <span class="md-list-item-text">
+              Arrivo
+              <b>{{convertTimestampToTime(viaggio.endtime)}}</b>
+            </span>
 
             <md-list slot="md-expand">
-              <md-list-item class="md-inset">World</md-list-item>
-              <md-list-item class="md-inset">Europe</md-list-item>
-              <md-list-item class="md-inset">South America</md-list-item>
-            </md-list>
-          </md-list-item>
-
-          <md-list-item md-expand>
-            <md-icon>videogame_asset</md-icon>
-            <span class="md-list-item-text">Games</span>
-
-            <md-list slot="md-expand">
-              <md-list-item class="md-inset">Console</md-list-item>
-              <md-list-item class="md-inset">PC</md-list-item>
-              <md-list-item class="md-inset">Phone</md-list-item>
+              <md-list-item
+                class="md-inset"
+                v-for="(item, index) in viaggio.leg"
+                :key="item.endtime"
+              >
+                <md-icon v-if="item.transport.type == 'WALK'">directions_walk</md-icon>
+                <md-icon v-if="item.transport.type == 'BUS'">directions_bus</md-icon>
+                <span class="md-list-item-text">{{index + 1}}) P: {{item.from.name}} | A: {{item.to.name}}</span>
+              </md-list-item>
             </md-list>
           </md-list-item>
         </md-list>
@@ -474,6 +484,35 @@ export default {
       }
     ]
   }),
+
+  methods: {
+    convertTimestampToTime(unix_timestamp) {
+      // Create a new JavaScript Date object based on the timestamp
+      // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+      var date = new Date(unix_timestamp);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+
+      // Will display time in 10:30:23 format
+      return hours + ":" + minutes.substr(-2);
+    },
+
+    msToHMS(ms) {
+      // 1- Convert to seconds:
+      var seconds = ms / 1000;
+      // 2- Extract hours:
+      var hours = parseInt(seconds / 3600); // 3,600 seconds in 1 hour
+      seconds = seconds % 3600; // seconds remaining after extracting hours
+      // 3- Extract minutes:
+      var minutes = parseInt(seconds / 60); // 60 seconds in 1 minute
+      // 4- Keep only seconds not extracted to minutes:
+      seconds = seconds % 60;
+
+      return hours + ":" + minutes + ":" + seconds;
+    }
+  },
   created: function() {
     if (this.risultati.length == 0) {
       Functions.pianificaViaggio(
