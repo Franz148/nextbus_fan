5aa05dfc373169c93517395839ebe04234bcfcd4
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-22 17:04:05 +0200

    Inserimento immagini da storage a database

--- src/database/db-functions.js
+++ src/database/db-functions.js
@@ -1,18 +1,43 @@
-import Firebase from "@firebase/app";
+import firebase from "@firebase/app";
 import "@firebase/firestore";
+import "@firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCVmCc7n4CmQwQ2srwxDzQL3tkBJ3sZhMI",
-    authDomain: "db-test2020.firebaseapp.com",
-    projectId: "db-test2020"
+    apiKey: "AIzaSyDlmXCeXTgEEgjlQJ1fRUJGITc1ujPlW7g",
+    authDomain: "nextbus-38e73.firebaseapp.com",
+    projectId: "nextbus-38e73",
+    storageBucket: "nextbus-38e73.appspot.com"
 };
 
-Firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-//var db = firebase.firestore();
-//var collUser = db.collection("users");
+var db = firebase.firestore();
+//var iconeLinee = db.collection("iconeLinee");
+
+//var storage = firebase.storage();
+//var storageRef = storage.ref();
 
 export default {
+    /*aggiuntaIconeLineeAlDatabase() {
+        storageRef.child("iconeLinee/").listAll().then(function(result) {
+            console.log(result);
+            result.items.forEach(function(imageRef) {
+                imageRef.getDownloadURL().then(function(url) {
+                    console.log(url);
+                    console.log(imageRef.name.split(".png")[0]);
+                    let id = imageRef.name.split(".png")[0]
+
+                    iconeLinee
+                        .doc()
+                        .set({
+                            idLinea: id,
+                            iconaLinea: url
+                        });
+                });
+            });
+        });
+    },*/
+
     login(username) {
         localStorage.setItem("username", username);
     },
