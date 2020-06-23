2a0960787aa9aa4db9a8aaba835dab37a97c8080
nicolemariz <61781935+nicolemariz@users.noreply.github.com>
2020-06-23 10:35:21 +0200

    Funzione per mostrare icone
    Aggiunto funzione per icone

--- src/views/Fermate.vue
+++ src/views/Fermate.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="md-layout md-gutter md-alignment-center-center">
     <md-list class="md-layout-item md-size-50">
+      <md-list class="md-layout-item md-size-80">
+       <div v-for="fermata in linea" :key="fermata.id">
+       <md-divider></md-divider>
+        <md-list-item>
+       <md-icon>directions_bus</md-icon>
+
+       <span class="md-list-item-text">{{fermata.name}}</span>
+
+        <md-icon class="md-primary" v-if="fermata.wheelChairBoarding == 1">accessible</md-icon>
+       <md-icon class="md-primary">favorite_border</md-icon>
+          </md-list-item>
+         </div>
+        </md-list>
       <md-divider></md-divider>
       <md-list-item>
-        <md-icon>directions_bus</md-icon>
+     
 
-        <span class="md-list-item-text">Piazza Dante Stazione FS</span>
-
-        <md-icon class="md-primary">accessible</md-icon>
-        <md-icon class="md-primary">favorite_border</md-icon>
-      </md-list-item>
-
-      <md-divider></md-divider>
+     
     </md-list>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  
+};
 </script>
 
 <style>
--- src/views/Linee.vue
+++ src/views/Linee.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="md-layout md-gutter md-alignment-center-center">
     <!-- <md-button class="md-primary md-raised" @click="ordinaLinee">Ordina</md-button> -->
+    <md-button class="md-primary md-raised" @click="mostraIconeLinee">Mostra</md-button>
     <md-list class="md-double-line md-layout-item md-size-50">
       <div v-for="linea in linee" :key="linea.id.id">
         <md-list-item :to="'/fermata/' + linea.id.id">
+          <md-avatar></md-avatar>
           <div class="md-list-item-text">
             <span>{{linea.id.id}}</span>
             <span>{{linea.routeLongName}}</span>
@@ -18,8 +20,13 @@
 
 <script>
 import Functions from "../api/functions.js";
+import "../database/db-functions.js";
+import dbFunctions from '../database/db-functions.js';
+
+
 
 export default {
+
   data: () => ({
     linee: []
   }),
@@ -31,7 +38,15 @@ export default {
       .catch(error => {
         console.error(error);
       });
+
+      dbFunctions.mostraIconeLinee("10R")
+      .then  (data => {
+        data.forEach(doc => {
+          console.log(doc.data());
+        });
+      });
   }
+  
   // methods: {
   //   ordinaLinee() { let confronta = (a, b) => {
   //     const nameRouteA = a.id.id.toUpperCase();
