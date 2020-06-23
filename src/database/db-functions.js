df23a05f4e6b3ade9c54522f8f90763466a93617
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-23 17:12:25 +0200

    Correzione e conclusione

--- src/database/db-functions.js
+++ src/database/db-functions.js
@@ -2,28 +2,34 @@ import firebase from "@firebase/app";
 import "@firebase/firestore";
 import "@firebase/storage";
 
+import Accesso from "../login/access-functions.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDlmXCeXTgEEgjlQJ1fRUJGITc1ujPlW7g",
     authDomain: "nextbus-38e73.firebaseapp.com",
-    projectId: "nextbus-38e73",
-    storageBucket: "nextbus-38e73.appspot.com"
+    projectId: "nextbus-38e73"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
-var iconeLinee = db.collection("iconeLinee");
-//var elementiPreferiti = db.collection("elementiPreferiti");
-
-//var storage = firebase.storage();
-//var storageRef = storage.ref();
+var elementiPreferiti = db.collection("elementiPreferiti");
 
 export default {
     getLineePreferite() {
+        var user = Accesso.getUsername().toLowerCase();
+        var arrayLinee = [];
 
-    },
+        return elementiPreferiti
+            .where("idFermata", "==", "-1")
+            .where("username", "==", user)
+            .get()
+            .then(results => {
+                results.forEach(doc => {
+                    arrayLinee.push(doc.data());
+                });
 
-    getFermatePreferite() {
-
+                return arrayLinee;
+            });
     }
 }
