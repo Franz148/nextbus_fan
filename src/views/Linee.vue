39140be01b8e319b9d38ceba2baedfb498989135
nicolemariz <61781935+nicolemariz@users.noreply.github.com>
2020-06-25 12:19:56 +0200

    Aggiunto funzione preferiti

--- src/views/Linee.vue
+++ src/views/Linee.vue
@@ -4,7 +4,9 @@
 
     <md-list class="md-double-line md-layout-item md-size-50">
       <div v-for="linea in linee" :key="linea.id.id">
-        <md-list-item :to="'/lineaSingola/' + linea.id.id + '?routeLongName='  + linea.routeLongName">
+        <md-list-item
+          :to="'/lineaSingola/' + linea.id.id + '?routeLongName='  + linea.routeLongName"
+        >
           <md-avatar>
             <img :src="getImageFromId(linea.id.id)" />
           </md-avatar>
@@ -12,8 +14,10 @@
             <span>{{linea.routeLongName}}</span>
             <span>{{linea.routeShortName}}</span>
           </div>
-         <md-button class="md-icon-button"> <md-icon >favorite_border</md-icon> </md-button> 
-         <!-- <md-button class="md-icon-button"> <md-icon >favorite</md-icon> </md-button>  -->
+          <md-button class="md-icon-button" v-if="linea.preferiti==0">
+            <md-icon>favorite_border</md-icon>
+          </md-button>
+          <md-button class="md-icon-button" v-if="linea.preferiti==1"> <md-icon >favorite</md-icon> </md-button> 
         </md-list-item>
         <md-divider></md-divider>
       </div>
@@ -23,6 +27,8 @@
 
 <script>
 import Functions from "../api/functions.js";
+import DBFunction from "../database/db-functions.js";
+import Accesso from "../login/access-functions.js";
 
 export default {
   data: () => ({
@@ -32,6 +38,8 @@ export default {
     Functions.getLinee(12)
       .then(results => {
         this.linee = results.data;
+        this.addFavoriteField(this.linee);
+        if (Accesso.isLoggedIn()) this.setFavorite(this.linee);
       })
       .catch(error => {
         console.error(error);
@@ -41,26 +49,49 @@ export default {
   methods: {
     getImageFromId(id) {
       return require("../assets/iconeLinee/" + id + ".png");
+    },
+    addFavoriteField(linee) {
+      linee.forEach(item => {
+        item.preferiti = 0;
+
+      });
+    },
+    setFavorite(linee) {
+      let lineePreferite = [];
+      DBFunction.getLineePreferite().then(results => {
+        lineePreferite = results;
+        console.log(linee);
+        console.log(lineePreferite);
+        for (let i = 0; i < linee.length; i++ ){
+          for (let i2 = 0; i2 < lineePreferite.length; i++ ){
+              if(linee[i].id.id == lineePreferite[i2].idLinea){
+                linee[i].preferiti=1;
+              }
+
+          }
+         
+        }
+
+      });
     }
   }
 
-//   ordinaLinee() { let confronta = (a, b) => {
-//   const nameRouteA = a.id.id.toUpperCase();
-//  const nameRouteB = b.id.id.toUpperCase();
+  //   ordinaLinee() { let confronta = (a, b) => {
+  //   const nameRouteA = a.id.id.toUpperCase();
+  //  const nameRouteB = b.id.id.toUpperCase();
 
-// let comparison = 0;
-//   if (nameRouteA > nameRouteB) {
-//    comparison = 1;
-//      } else if (nameRouteA < nameRouteB) {
-//         comparison = -1;
-//      }
-//       return comparison;
-//  }
-//       this.linee.sort(confronta);
-//    console.log(this.linee);
-//      }
-  
- };
+  // let comparison = 0;
+  //   if (nameRouteA > nameRouteB) {
+  //    comparison = 1;
+  //      } else if (nameRouteA < nameRouteB) {
+  //         comparison = -1;
+  //      }
+  //       return comparison;
+  //  }
+  //       this.linee.sort(confronta);
+  //    console.log(this.linee);
+  //      }
+};
 </script>
 
 <style>
