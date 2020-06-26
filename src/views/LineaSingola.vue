ccada01fc6a1de6255e6f1156e3fa493e55fb7b4
alberto-testa <61781951+alberto-testa@users.noreply.github.com>
2020-06-26 17:18:14 +0200

    controllo orari non disponibili e sostituzione id

--- src/views/LineaSingola.vue
+++ src/views/LineaSingola.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="md-layout md-gutter md-alignment-center-center">
-    <!-- <div class="md-layout-item md-size-100">
-      <md-avatar>
-          <img src="https://placeimg.com/40/40/people/5" alt="People">
-        </md-avatar>
-      <h1>nome linea</h1>
-    </div>-->
-
     <md-card class="md-layout-item md-size-50">
       <md-card-header>
         <div class="md-layout md-gutter">
@@ -17,49 +10,57 @@
           </div>
           <div class="md-layout-item md-size-85">
             <div class="md-title">{{$route.query.routeLongName}}</div>
-            
           </div>
         </div>
       </md-card-header>
+      <md-card-expand>
+        <md-card-actions md-alignment="space-between">
+          <div>
+            <md-button
+              v-bind:disabled="isButtonPrecedenteDisabled"
+              v-on:click="precedente"
+            >Precedente</md-button>
+          </div>
+          <div>
+            <md-button v-bind:disabled="isButtonSuccessivoDisabled" @click="successivo">Successivo</md-button>
+          </div>
+        </md-card-actions>
+      </md-card-expand>
     </md-card>
 
+    <!--AVVISO ORARI NON DISPONIBILI-->
+    <div class="md-layout-item md-size-100" v-show="orariNonDisponibili">
+      <span>Orari non disponibili</span>
+    </div>
+
     <md-list class="md-layout-item md-size-50">
-      <!-- <div v-for="n in idFermate.length-1" :key="idFermate[n]"> -->
       <div v-for="(id,n) in idFermate" :key="idFermate[n]">
-        <!--fermata abilitata-->
-        <!--passa = trip[n] se "" allora non passa-->
-        <template v-if="viaggi[n] !=''">
+        <!--FERMATA DOVE PASSA IL BUS-->
+        <!--passa se orari[n] != '' -->
+        <template v-if="orari[n] !=''">
           <md-list-item>
             <md-icon class="md-primary">directions_bus</md-icon>
-
             <div class="md-list-item-text">
               <span>{{nomeFermate[n]}}</span>
-              <span>Orario previsto: {{viaggi[n]}}</span>
-              <!-- <span>{{accessibilita[n].wheelChairBoarding}}</span> -->
-              <!-- <span>Orario previsto: {{accessibilita[n].wheelChairBoarding}}</span> -->
-              <!-- <span>{{ritornaAccessibile(accessibilita[n])}}</span> -->
+              <span>Orario previsto: {{orari[n]}}</span>
             </div>
-
-            <md-icon class="md-primary" v-if="ritornaAccessibile(accessibilita,n) != 0">accessible</md-icon>
-            <!-- <md-icon class="md-primary" v-if="ritornaAccessibile(accessibilita) != 0">accessible</md-icon> -->
-            <!-- <md-icon class="md-primary">favorite_border</md-icon> -->
+            <!-- 0=non accessibile, 1 accessibile,2 non si hanno informazioni-->
+            <md-icon class="md-primary" v-if="ritornaAccessibile(accessibilita,n) ==1">accessible</md-icon>
           </md-list-item>
         </template>
 
-        <!--fermata non abilitata-->
-
+        <!--FERMATA DOVE NON PASSA IL BUS-->
         <template v-else>
           <md-list-item>
             <md-icon>directions_bus</md-icon>
             <div class="md-list-item-text">
               <span>{{nomeFermate[n]}}</span>
-              <!-- <span>{{accessibilita[n].wheelChairBoarding}}</span> -->
+              <!-- <span>{{ritornaAccessibile(accessibilita,n)}}</span> -->
             </div>
-            <md-icon v-if="ritornaAccessibile(accessibilita,n) != 0">accessible</md-icon>
+            <md-icon v-if="ritornaAccessibile(accessibilita,n) ==1">accessible</md-icon>
             <!-- <md-icon v-if="ritornaAccessibile(accessibilita[n]) != 0">accessible</md-icon> -->
           </md-list-item>
         </template>
-
         <md-divider></md-divider>
       </div>
     </md-list>
@@ -68,50 +69,94 @@
 
 <script>
 import Functions from "../api/functions.js";
-//import functions from '../api/functions.js';
 export default {
   data: () => ({
     idFermate: [],
     nomeFermate: [],
-    viaggi: [],
+    orari: [],
     accessibilita: [],
     accessibile: [],
     idAgency: 12,
-    idRoutes: null
+    idRoutes: null,
+    viaggi: [],
+    current: 0,
+    isButtonPrecedenteDisabled: false,
+    isButtonSuccessivoDisabled: false,
+    orariNonDisponibili: false
   }),
 
   beforeCreate: function() {
-    
     console.log(this.$route.params.id);
-    console.log(this.$route.query.routeLongName);
+    //console.log(this.$route.query.routeLongName);
+    //this.orari=this.viaggi[0].stopTimes;
   },
   created: function() {
-    this.idRoutes = this.$route.params.id;
-    //(idAgency,idRotes)
-    Functions.getLineaSingolaConOrario(this.idAgency, this.$route.params.id)
+    //this.idRoutes = this.$route.params.id;
+    //SOSTITUISCO GLI ID INESISTENTI PASSATI DA LINEE
+    switch (this.$route.params.id) {
+      case "1A":
+        this.idRoutes = "01A";
+        break;
+      case "1R":
+        this.idRoutes = "01R"; //{"stopIds":null,"stopNames":null,"trips":[]}
+        break;
+      case "02":
+        this.idRoutes = "02C";
+        break;
+      case "_A":
+        this.idRoutes = "%20AC"; //NON VA
+        break;
+      case "FUTSR":
+        this.idRoutes = "FunR";
+        break;
+      case "FUTSA":
+        this.idRoutes = "FunA";
+        break;
+      case "GR":
+        this.idRoutes = "%20GR"; //NON VA
+        break;
+      case "GA":
+        this.idRoutes = "%20GA"; //NON VA
+        break;
+      case "NPA":
+        this.idRoutes = "NPC";
+        break;
+
+      default:
+        this.idRoutes = this.$route.params.id;
+    }
+
+    Functions.getLineaSingolaConOrario(this.idAgency, this.idRoutes)
       .then(results => {
         this.idFermate = results.data.stopIds;
         this.nomeFermate = results.data.stopNames;
-        console.log(
+        /* console.log(
           "id fermate:" +
             this.idFermate.length +
             " nome fermate:" +
             this.nomeFermate.length
-        );
+        ); */
 
-        //la posizione di results.data.trips è la trip
-        this.viaggi = results.data.trips[1].stopTimes;
+        //la posizione di results.data.trips è l'array di trips
+
+        console.log(results.data.trips.length);
+        this.viaggi = results.data.trips;
+        console.log(this.viaggi);
+        //all'avvio carico di default gli orari del primo viaggio
+        this.orari = this.viaggi[0].stopTimes;
+        //console.log(this.orari);
       })
       .catch(error => {
         console.error(error);
+        // orari non disponibili perchè l'array è vuoto
+        this.orariNonDisponibili = true;
       });
 
     Functions.getLineaSingolaAccessibilita(this.idAgency, this.idRoutes)
       .then(results2 => {
         this.accessibilita = results2.data;
-        console.log("accessibilità" + " " + this.accessibilita.length);
-        console.log(this.accessibilita);
-        //console.log(this.accessibilita[23].wheelChairBoarding);
+        /* console.log("accessibilità" + " " + this.accessibilita.length);
+        console.log(this.accessibilita); */
       })
       .catch(error => {
         console.log("accessibilità" + " " + this.accessibilita.length);
@@ -119,8 +164,36 @@ export default {
       });
   },
   methods: {
-    
     getImageFromId(id) {
+      switch (id) {
+        case "01A":
+          id = "1A";
+          break;
+        case "01R":
+          id = "1R";
+          break;
+        case "02C":
+          id = "02";
+          break;
+        case "%20AC":
+          id = "_A";
+          break;
+        case "FunR":
+          id = "FUTSR";
+          break;
+        case "FunA":
+          id = "FUTSA";
+          break;
+        case "%20GR":
+          id = "GR";
+          break;
+        case "%20GA":
+          id = "GA";
+          break;
+        case "NPC":
+          id = "NPA";
+          break;
+      }
       return require("../assets/iconeLinee/" + id + ".png");
     },
     /* ritornaAccessibile(accessibile)
@@ -138,11 +211,40 @@ export default {
           console.log("non va");
         } */
       }
+    },
+    //DA CORREGGERE CHE IL PRIMO E L'ULTIMO POSSO CLICCARLI 2 VOLTE
+    precedente() {
+      this.isButtonSuccessivoDisabled = false;
+      if (this.current <= 0) {
+        //precedente disabilitato
+        this.isButtonPrecedenteDisabled = true;
+      } else {
+        //controllo i bottoni da disabilitare
+        console.log("precedente");
+        this.current -= 1;
+        console.log(this.current);
+        this.orari = this.viaggi[this.current].stopTimes;
+        //this.isButtonPrecedenteDisabled=false;
+      }
+    },
+    successivo() {
+      this.isButtonPrecedenteDisabled = false;
+
+      if (this.current >= this.viaggi.length - 1) {
+        //successivo disabilitato
+        this.isButtonSuccessivoDisabled = true;
+      } else {
+        //controllo i bottoni da disabilitare
+        console.log("successivo");
+        this.current += 1;
+        console.log(this.current);
+        this.orari = this.viaggi[this.current].stopTimes;
+        //this.isButtonSuccessivoDisabled=false;
+      }
     }
   }
 };
 </script>
-
 
 <style>
 </style>
