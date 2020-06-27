b9fcf60ac55645441f58ca6f1052015c69c59f8f
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-06-27 16:58:53 +0200

    Gestione Spinner, controllo DB in preferiti.vue

--- src/views/Preferiti.vue
+++ src/views/Preferiti.vue
@@ -18,29 +18,48 @@
         <md-card-header>
           <md-card-header-text>
             <div class="md-title">Lista dei preferiti</div>
-            <div class="md-subhead">
-              Linee preferite verranno visualizzate qui
-            </div>
+            <div class="md-subhead">Le linee preferite verranno visualizzate qui</div>
           </md-card-header-text>
         </md-card-header>
 
         <md-card-content class="content">
-          <md-list v-if="lineePreferite.length != 0">
-            <div v-for="linea in lineePreferite" :key="linea.idLinea">
-              <md-list-item :to="'/lineaSingola/' + linea.idLinea + '?routeLongName=' + returnLineaSingola(linea.idLinea).routeLongName">
-                <md-avatar>
-                  <img :src="getImageFromId(linea.idLinea)" />
-                </md-avatar>
-                <span class="md-list-item-text">{{returnLineaSingola(linea.idLinea).routeLongName}}</span>
-
-                <md-button class="md-icon-button md-list-action">
-                  <md-icon class="md-primary">favorite</md-icon>
-                </md-button>
-              </md-list-item>
-              <md-divider></md-divider>
+          <div class="md-layout md-alignment-center-center">
+            <div class="md-layout-item md-size-5">
+              <md-progress-spinner md-mode="indeterminate" v-show="activeSpinner"></md-progress-spinner>
             </div>
-          </md-list>
-          <span v-if="lineePreferite.length == 0">Non sono presenti preferiti</span>
+
+            <md-list v-if="!shownedEmptyState" class="md-layout-item md-size-100">
+              <div v-for="(linea, i) in lineePreferite" :key="linea.idLinea">
+                <md-list-item
+                  :to="'/lineaSingola/' + linea.idLinea + '?routeLongName=' + returnLineaSingola(linea.idLinea).routeLongName"
+                >
+                  <md-avatar>
+                    <img :src="getImageFromId(linea.idLinea)" />
+                  </md-avatar>
+                  <span
+                    class="md-list-item-text"
+                  >{{returnLineaSingola(linea.idLinea).routeLongName}}</span>
+
+                  <md-button
+                    class="md-icon-button md-primary"
+                    @click.stop.prevent="addFavoriteLine(linea.idLinea, 1,i)"
+                  >
+                    <md-icon>favorite</md-icon>
+                  </md-button>
+                </md-list-item>
+                <md-divider></md-divider>
+              </div>
+            </md-list>
+
+            <md-empty-state
+              md-icon="favorite_border"
+              md-label="Non hai ancora preferiti"
+              md-description="Se vuoi visualizzare qui le linee che consulti più spesso vai alla pagina Linee."
+              v-else
+            >
+              <md-button class="md-primary md-raised" to="/linee">Pagina linee</md-button>
+            </md-empty-state>
+          </div>
         </md-card-content>
       </md-card>
     </div>
@@ -55,7 +74,9 @@ import Functions from "../api/functions.js";
 export default {
   data: () => ({
     datiLinee: [],
-    lineePreferite: []
+    lineePreferite: [],
+    activeSpinner: true,
+    shownedEmptyState: false
   }),
   methods: {
     checkAutenticazione() {
@@ -82,12 +103,24 @@ export default {
       }
 
       return linea[0];
+    },
+
+    addFavoriteLine(id, preferiti, i) {
+      if (preferiti == 1) {
+        Dbfunctions.rimuoviLineaPreferita(id).then(() => {
+          this.lineePreferite.splice(i, 1);
+          if (this.lineePreferite.length == 0) this.shownedEmptyState = true;
+        });
+      }
     }
   },
 
-  beforeCreate: function() {
+  created: function() {
     Dbfunctions.getLineePreferite().then(results => {
       this.lineePreferite = results;
+      this.activeSpinner = false;
+
+      if (this.lineePreferite.length == 0) this.shownedEmptyState = true;
     });
 
     Functions.getLinee(12).then(results => {
