fa056bd38b19049fb721e226cc273e749549178d
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-07-04 15:44:59 +0200

    Commenti e sistemazione codice

--- src/api/functions.js
+++ src/api/functions.js
@@ -1,80 +1,30 @@
 import axios from "axios";
 
 export default {
-    getLinee(agencyId) {
-        //this.linee = results.data;
 
+    //RESTITUISCE LA LISTA DELLE LINEE DI UNO SPECIFICO SERVIZIO (ES. Autobus urbani trenti agencyId = 12 )
+    getLinee(agencyId) {
         return axios.get("https://os.smartcommunitylab.it/core.mobility/getroutes/" + agencyId);
     },
 
+    //RESTITUISCE LA LISTA DELLE DELLE FERMATE DI UNA LINEA CON GLI ORARI COMPLETI DI UN GIORNO
     getLineaSingolaConOrario(agencyId, routeId) {
-        /*this.idFermate = results.data.stopIds;
-        this.nomeFermate = results.data.stopNames;
-        this.viaggi = results.data.trips[0].stopTimes;*/
-
-        //     <md-list>
-        //   <div v-for="n in idFermate.length" :key="idFermate[n]">
-        //     <md-list-item v-if="funzioneProva(viaggi[n])">
-        //       <div class="md-list-item-text">
-        //         <span>{{idFermate[n]}}</span>
-        //         <span>{{nomeFermate[n]}}</span>
-        //         <span>{{viaggi[n]}}</span>
-        //       </div>
-        //     </md-list-item>
-        //     <md-divider></md-divider>
-        //   </div>
-        // </md-list>
-
         return axios.get("https://os.smartcommunitylab.it/core.mobility/timetable/" + agencyId + "/" + routeId);
     },
 
+    //RESTITUISCE LA LISTA DELLE FERMATE DI UNA LINEA CON DATI COME ID, NOME, ACCESSIBILITÀ, LAT E LNG 
     getLineaSingolaAccessibilita(agencyId, routeId) {
-
-        //this.linea = results.data
-
-        //     <md-list class="md-layout-item md-size-80">
-        //   <div v-for="fermata in linea" :key="fermata.id">
-        //     <md-divider></md-divider>
-        //     <md-list-item>
-        //       <md-icon>directions_bus</md-icon>
-
-        //       <span class="md-list-item-text">{{fermata.name}}</span>
-
-        //       <md-icon class="md-primary" v-if="fermata.wheelChairBoarding == 1">accessible</md-icon>
-        //       <md-icon class="md-primary">favorite_border</md-icon>
-        //     </md-list-item>
-        //   </div>
-        // </md-list>
         return axios.get("https://os.smartcommunitylab.it/core.mobility/getstops/" + agencyId + "/" + routeId);
     },
 
+    //RESTITUISCE L'ARRAY CON SOLO LA LISTA DEGLI ORARI (TRIPS) DATA LA LINEA SPECIFICA
     getArrayOrarioTotaleLineaSingola(agencyId, routeId) {
-
-        /*
-        <md-field class="md-layout-item md-size-80">
-      <label>Quale orario vuoi?</label>
-      <md-input type="number" v-model="numOrario"></md-input>
-    </md-field>
-
-    <p class="md-layout-item md-size-80">Orario selezionato: {{numOrario}} | Orari totali: {{orari.length - 1}}</p>
-
-    <md-list class="md-layout-item md-size-80" v-if="numOrario != null ">
-      <md-list-item v-for="orarioSingolo in orari[numOrario].stopTimes" :key="orarioSingolo">
-        <md-icon>move_to_inbox</md-icon>
-        <span class="md-list-item-text">{{orarioSingolo}}</span>
-      </md-list-item>
-    </md-list>
-
-        */
-
-        //this.orari = data;
-
         return this.getLineaSingolaConOrario(agencyId, routeId).then(results => {
             return results.data.trips;
         });
     },
 
-    //API opencagedata
+    //API OPENCAGEDATA --> RESTITUISCE DATI GEOGRAFICI COME LAT E LNG PASSANDO UNA STRINGA (VIENE RISTRETTO IL CAMPO A SOLO RISULTATI IN ITALIANO E GEOGRAFICAMENTE IN ITALIA)
     getRisultatiGeograficiDaInserimento(userInput) {
         return axios.get("https://api.opencagedata.com/geocode/v1/json", {
             params: {
@@ -86,7 +36,7 @@ export default {
         });
     },
 
-    //Restituisce le possibili strade da percorrere 
+    //RESTITUISCE UNA SINGLE JOURNEY STRUCTURE PASSANDO LAT E LNG DI PARTENZA E ARRIVO, DATA E ORA DI PARTENZA
     getRisultatiPercorso(partenza, arrivo, dataPartenza, oraPartenza) {
         return axios.post("https://dev.smartcommunitylab.it/core.mobility/plansinglejourney/", {
             "from": {
@@ -108,7 +58,7 @@ export default {
         });
     },
 
-    //Due richieste contemporanee A/R da indirizzi
+    //ESEGUE E RESTITUISCE IN UNA VOLTA DUE RICHIESTE PER CONVERTIRE I DUE INDIRIZZI IN COORDINATE GEOGRAFICHE
     richiestaDatiPA(indirizzoPartenza, indirizzoArrivo) {
         const richiestaDatiPartenza = this.getRisultatiGeograficiDaInserimento(indirizzoPartenza);
         const richiestaDatiArrivo = this.getRisultatiGeograficiDaInserimento(indirizzoArrivo);
@@ -118,6 +68,7 @@ export default {
         }));
     },
 
+    //FUNZIONE CHE RACCHIUDE LA CONVERSIONE DEGLI INDIRIZZI (PASSANDO L'INPUT DELL'UTENTE INDIRIZZI, DATA E ORA DELLA PARTENZA) E I RISULTATI IN FORMATO SINGLE JOURNEY STRUCTURE
     pianificaViaggio(indirizzoPartenza, indirizzoArrivo, dataPartenza, oraPartenza) {
         let partenza = { lat: "", lng: "" };
         let arrivo = { lat: "", lng: "" };
@@ -126,6 +77,7 @@ export default {
             .then(responses => {
                 console.warn("Richieste rimanenti opencagedata.com: " + responses[1].data.rate.remaining);
 
+                //La funzione di conversione degli indirizzi restituisce più risultati, per comodità diamo per scontato che il primo sia corretto (per questo si usa data.results[0])
                 partenza.lat = responses[0].data.results[0].geometry.lat;
                 partenza.lng = responses[0].data.results[0].geometry.lng;
 
