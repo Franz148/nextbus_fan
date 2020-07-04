3782a934e814817b71f74057a8a6029d8b00af3c
Francisco Guarnieri <franzguarnieri148@gmail.com>
2020-07-04 11:36:04 +0200

    Layout, empty state e salva ricerca

--- src/views/Viaggio.vue
+++ src/views/Viaggio.vue
@@ -1,83 +1,87 @@
 <template>
   <div class="md-layout-item md-size-100">
-    <md-card class="md-layout-item md-size-90">
-      <md-card-header>
-        <div class="md-title">Risultati della ricerca</div>
-      </md-card-header>
+    <div class="md-layout md-alignment-center-center">
+      <div class="md-layout-item md-size-100 md-title">Risultati della ricerca</div>
+      <div class="md-layout-item md-size-10">
+        <md-progress-spinner class="md-layout-item" md-mode="indeterminate" v-show="activeSpinner"></md-progress-spinner>
+      </div>
 
-      <md-card-content class="md-layout md-alignment-center-center">
-        <div class="md-layout-item md-size-5">
-          <md-progress-spinner md-mode="indeterminate" v-show="activeSpinner"></md-progress-spinner>
-        </div>
+      <md-list md-expand-single="true" class="md-layout-item md-size-100 md-triple-line">
+        <div v-for="(viaggio, i) in risultati" :key="i">
+          <md-list-item md-expand>
+            <md-icon v-if="viaggio.walkingDuration == (viaggio.duration / 1000)">directions_walk</md-icon>
+            <md-icon v-if="viaggio.walkingDuration != (viaggio.duration / 1000)">directions_bus</md-icon>
+            <div class="md-list-item-text">
+              <span>
+                Partenza
+                <b>{{convertTimestampToTime(viaggio.startime)}}</b>
+              </span>
 
-        <md-list md-expand-single="true" class="md-layout-item md-size-100 md-triple-line">
-          <div v-for="(viaggio, i) in risultati" :key="i">
-            <md-list-item md-expand>
-              <md-icon v-if="viaggio.walkingDuration == (viaggio.duration / 1000)">directions_walk</md-icon>
-              <md-icon v-if="viaggio.walkingDuration != (viaggio.duration / 1000)">directions_bus</md-icon>
-              <div class="md-list-item-text">
-                <span>
-                  Partenza
-                  <b>{{convertTimestampToTime(viaggio.startime)}}</b>
-                </span>
+              <span>
+                Arrivo
+                <b>{{convertTimestampToTime(viaggio.endtime)}}</b>
+              </span>
 
-                <span>
-                  Arrivo
-                  <b>{{convertTimestampToTime(viaggio.endtime)}}</b>
-                </span>
+              <span>
+                Durata
+                <b>{{msToHMS(viaggio.duration)}}</b>
+              </span>
+            </div>
 
-                <span>
-                  Durata
-                  <b>{{msToHMS(viaggio.duration)}}</b>
-                </span>
-              </div>
-
-              <md-list slot="md-expand" class="md-triple-line">
-                <md-list-item
-                  class="md-inset"
-                  v-for="(item, index) in viaggio.leg"
-                  :key="item.endtime"
+            <md-list slot="md-expand" class="md-triple-line">
+              <md-list-item
+                class="md-inset"
+                v-for="(item, index) in viaggio.leg"
+                :key="item.endtime"
+              >
+                <md-avatar class="md-avatar-icon" v-if="item.transport.type == 'WALK'">
+                  <md-icon>directions_walk</md-icon>
+                </md-avatar>
+                <md-avatar
+                  class="md-avatar-icon"
+                  v-if="item.transport.type == 'BUS' && item.transport.agencyId != 12"
                 >
-                  <md-avatar class="md-avatar-icon" v-if="item.transport.type == 'WALK'">
-                    <md-icon>directions_walk</md-icon>
-                  </md-avatar>
-                  <md-avatar
-                    class="md-avatar-icon"
+                  <md-icon>directions_bus</md-icon>
+                </md-avatar>
+                <md-avatar v-if="item.transport.type == 'BUS' && item.transport.agencyId == 12">
+                  <img :src="getImageFromId(item.transport.routeId)" />
+                </md-avatar>
+                <md-avatar class="md-avatar-icon" v-if="item.transport.type == 'TRAIN'">
+                  <md-icon>train</md-icon>
+                </md-avatar>
+                <div class="md-list-item-text">
+                  <span
                     v-if="item.transport.type == 'BUS' && item.transport.agencyId != 12"
-                  >
-                    <md-icon>directions_bus</md-icon>
-                  </md-avatar>
-                  <md-avatar v-if="item.transport.type == 'BUS' && item.transport.agencyId == 12">
-                    <img :src="getImageFromId(item.transport.routeId)" />
-                  </md-avatar>
-                  <md-avatar class="md-avatar-icon" v-if="item.transport.type == 'TRAIN'">
-                    <md-icon>train</md-icon>
-                  </md-avatar>
-                  <div class="md-list-item-text">
-                    <span
-                      v-if="item.transport.type == 'BUS' && item.transport.agencyId != 12"
-                    >{{index + 1}}) Linea: {{item.transport.routeShortName}} | P: {{item.from.name}}</span>
-                    <span v-else>{{index + 1}}) P: {{item.from.name}}</span>
-                    <span>A: {{item.to.name}}</span>
+                  >{{index + 1}}) Linea: {{item.transport.routeShortName}} | P: {{item.from.name}}</span>
+                  <span v-else>{{index + 1}}) P: {{item.from.name}}</span>
+                  <span>A: {{item.to.name}}</span>
 
-                    <span>Partenza alle: {{convertTimestampToTime(item.startime)}}</span>
-                  </div>
-                </md-list-item>
-              </md-list>
-            </md-list-item>
-            <md-divider></md-divider>
-          </div>
-        </md-list>
+                  <span>Partenza alle: {{convertTimestampToTime(item.startime)}}</span>
+                </div>
+              </md-list-item>
+            </md-list>
+          </md-list-item>
+          <md-divider></md-divider>
+        </div>
+      </md-list>
+    </div>
 
-        <md-empty-state
-          md-rounded
-          md-icon="error"
-          md-label="C'è stato un errore nella ricerca"
-          md-description="Clicca su riprova e reinserisci la partenza e la destinazione."
-          v-if="showSBErrore"
-        ></md-empty-state>
-      </md-card-content>
-    </md-card>
+    <md-empty-state
+      md-rounded
+      md-icon="error"
+      md-label="C'è stato un errore nella ricerca"
+      md-description="Clicca su riprova e reinserisci la partenza e la destinazione."
+      v-if="showSBErrore"
+    ></md-empty-state>
+
+    <md-empty-state
+      md-rounded
+      md-icon="search_off"
+      md-label="La ricerca non ha prodotto risultati"
+      md-description="Prova a cambiare partenza e arrivo"
+      v-if="zeroRisultati"
+    ></md-empty-state>
+
     <md-snackbar :md-active.sync="showSBErrore" :md-duration="duration">
       <span>La ricerca non è andata a buon fine!</span>
       <md-button class="md-primary" @click="ricercaFallita()">Riprova</md-button>
@@ -86,7 +90,9 @@
 </template>
 
 <script>
+import Accesso from "../login/access-functions.js";
 import Functions from "../api/functions.js";
+import dbFunctions from '../database/db-functions';
 var _ = require("lodash");
 
 export default {
@@ -98,7 +104,8 @@ export default {
     oraPartenza: "",
     activeSpinner: true,
     showSBErrore: false,
-    duration: Infinity
+    duration: Infinity,
+    zeroRisultati: false
   }),
 
   methods: {
@@ -166,6 +173,15 @@ export default {
           this.activeSpinner = false;
 
           this.risultati = results.data;
+
+          if (results.data.length == 0) this.zeroRisultati = true;
+          if (results.data.length != 0) {
+            this.zeroRisultati = false;
+
+            if(Accesso.isLoggedIn()){
+              dbFunctions.salvaUltimaRicerca(this.indirizzoA, this.indirizzoP, this.dataPartenza, this.$route.query.oraPartenza);
+            }
+          }
         })
         .catch(() => {
           this.showSBErrore = true;
@@ -186,6 +202,7 @@ export default {
   watch: {
     $route(from, to) {
       if (_.isEqual(from.query, to.query) == false) {
+        this.activeSpinner = true;
         this.loadViaggio();
       }
     }
